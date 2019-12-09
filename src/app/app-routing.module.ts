b6438e59e17056533e26f98c7df99ad313983846
Juan <juangonzaleze04@gmail.com>
2019-12-09 06:25:12 +0100

    fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+
   {
     path: '',
     loadChildren: () => import('./pages/login/login.module').then(m => m.LoginPageModule)
+  },
+    {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'home',
